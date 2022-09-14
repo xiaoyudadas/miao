696c435c2ccc1cddb154ab63851f6c60e07c0199
xiaoyudada <xiaoyu@example.com>
2022-09-14 17:29:41 +0800

    ceshi

---
+++ lodash/xiaoyudadas-lodash.js
@@ -0,0 +1,16 @@
+var xiaoyudadas = {
+  chunk: {
+    function(array, size = 1) {
+      var temp = new Array(Math.ceil(array.length / size))
+      var a = 0
+      for (var i = 0; i < array.length; i++) {
+        temp[a] ? temp[a] : temp[a] = []
+        temp[a].push(array[i])
+        if (temp[a].length == size) {
+          a++
+        }
+      }
+      return temp
+    }
+  }
+}
